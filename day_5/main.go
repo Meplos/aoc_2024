fca75c506a4f22d3d14319d5bad4475ce462b2f3
Meplos <alexxd64150@gmail.com>
2024-12-05 19:01:57 +0100

    chore: d5p1

--- day_5/main.go
+++ day_5/main.go
@@ -4,12 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
+)
+
+var (
+	rules    = make(map[int][]int)
+	goodList = make([][]int, 0)
 )
 
 func main() {
 	fmt.Println(" ==== Day 5 ====")
-	file := Must(os.Open("input/sample.txt"))
+	file := Must(os.Open("input/part1.txt"))
 	scanner := bufio.NewScanner(file)
 
 	scanner.Split(bufio.ScanLines)
@@ -27,15 +34,46 @@ func main() {
 			ParseRules(text)
 		}
 	}
+	fmt.Println(goodList)
+	counter := 0
+	for _, currentList := range goodList {
+		size := len(currentList)
+		mid := size / 2
+		counter += currentList[mid]
+	}
+	fmt.Printf("Result : %v\n", counter)
 }
 
 func ParseUpdateList(str string) {
-	list := strings.Split(str, ",")
-	fmt.Println(list)
+	list := ConvertToIntList(strings.Split(str, ","))
+	for curr, val := range list {
+		for i := 0; i < curr; i++ {
+			if slices.Contains(rules[val], list[i]) {
+				return
+			}
+		}
+	}
+
+	goodList = append(goodList, list)
+}
+
+func ConvertToIntList(arr []string) []int {
+	list := make([]int, 0)
+	for _, val := range arr {
+		list = append(list, int(Must(strconv.ParseInt(val, 0, 64))))
+	}
+
+	return list
 }
 
 func ParseRules(str string) {
 	list := strings.Split(str, "|")
+	x := int(Must(strconv.ParseInt(list[0], 0, 64)))
+	y := int(Must(strconv.ParseInt(list[1], 0, 64)))
+	if rules[x] == nil {
+		rules[x] = make([]int, 0)
+	}
+	rules[x] = append(rules[x], y)
 	fmt.Println(list)
 }
 
